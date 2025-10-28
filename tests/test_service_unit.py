7d53c20ee649976188ff0ca0e27b1455b0187449
Venkat Gorla <venkatagorla@gmail.com>
2025-10-28 11:30:20 +0530

    Unit test(service): add unit test for add_friendship to complete service UT coverage

--- tests/test_service_unit.py
+++ tests/test_service_unit.py
@@ -1,5 +1,5 @@
 from unittest.mock import MagicMock
-from social_graph.models import User
+from social_graph.models import User, Friendship
 from social_graph import service
 
 def test_add_user_with_injected_driver():
@@ -10,6 +10,29 @@ def test_add_user_with_injected_driver():
 
     mock_driver.run_query.assert_called_once()
     assert result == [{"username": "alice"}]
+
+def test_add_friendship_executes_expected_query():
+    # Arrange
+    mock_driver = MagicMock()
+    mock_driver.run_query.return_value = [
+        {"user1": "alice", "user2": "bob"},
+    ]
+    friendship = Friendship(user1="alice", user2="bob")
+
+    # Act
+    result = service.add_friendship(friendship, driver=mock_driver)
+
+    # Assert
+    mock_driver.run_query.assert_called_once_with(
+        """
+    MATCH (a:User {username: $user1}), (b:User {username: $user2})
+    MERGE (a)-[:FRIEND_WITH]->(b)
+    MERGE (b)-[:FRIEND_WITH]->(a)
+    RETURN a.username AS user1, b.username AS user2
+    """,
+        {"user1": "alice", "user2": "bob"},
+    )
+    assert result == [{"user1": "alice", "user2": "bob"}]
 
 def test_list_friends_returns_sorted_list():
     # Arrange
